5ade12919a17f95b12b58c72fdfd7144653373ef
Edilberto <edilbertoa.a.ses@gmail.com>
2026-06-06 07:48:08 -0400

    actualizacion de la carpeta de guardado de imagenes 12

--- src/servicios/supabase.js
+++ src/servicios/supabase.js
@@ -1,5 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
+console.log("URL FRONT:", import.meta.env.VITE_SUPABASE_URL);
+console.log("KEY FRONT:", import.meta.env.VITE_SUPABASE_KEY);
+
+
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_KEY
