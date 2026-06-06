375b6a45cff6a6c89bf44388660e541b67ff5bea
Edilberto <edilbertoa.a.ses@gmail.com>
2026-06-06 08:19:05 -0400

    actualizacion de la carpeta de guardado de imagenes 15 final 3

--- netlify/functions/guardarUsuario.js
+++ netlify/functions/guardarUsuario.js
@@ -1,4 +1,4 @@
-import { supabase } from '../servicios/supabase.js';
+import { supabase } from './supabase.js';
 import { validarNombre, validarContraseña, validarTelefono } from '../utilidades/validaciones.js';
 
 export async function handler(event) {
